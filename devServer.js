01b93f94fa302222a2d3b58dfcd997f43e968daf
Alexandre Beaudoin <ofrogon@gmail.com>
2014-10-04 23:01:26 -0400

    Setup the project to run clean-bower-installer.
    Now when the devServer.js start, the libs where updated just before.

--- devServer.js
+++ devServer.js
@@ -1,10 +1,16 @@
 var express = require('express'),
+	exec = require('child_process').exec,
 	app = express(),
 	open = require("open"),
 	port = 64242;
 
+
 app.use(express.static(__dirname + '/public'));
 
-open('http://localhost:' + port);
+// Get the bower libs, install them and start preview
+exec('clean-bower-install -i', function () {
+	open('http://localhost:' + port);
+});
+
 
 app.listen(port);
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-npm install -g bower
 npm install -g less
 npm install -g clean-bower-installer
